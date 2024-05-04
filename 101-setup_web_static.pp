a0ef6e65d8270a3e5329a565d388516da40bdcc6
Mackay89 <manotomackay@gmail.com>
2024-05-04 15:15:33 -0700

    task

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,42 +1,36 @@
-exec { 'sudo_apt-get_update':
+exec { 'sudo apt-get-update':
   command => '/usr/bin/apt-get -y update',
 }
 
-exec { 'install_nginx':
+exec { 'b':
   command => '/usr/bin/apt-get -y install nginx',
-  require => Exec['sudo_apt-get_update'],
+  require => Exec['sudo apt-get-update']
 }
 
-exec { 'create_test_directory':
+exec { 'c':
   command => '/bin/mkdir -p /data/web_static/releases/test/',
 }
 
-exec { 'create_shared_directory':
+exec { 'd':
   command => '/bin/mkdir -p /data/web_static/shared/',
 }
 
-file { '/data/web_static/releases/test/index.html':
-  ensure => present,
-  content => 'Puppet x School',
+exec { 'e':
+  command => '/bin/echo "Puppet x School" > /data/web_static/releases/test/index.html',
 }
 
-file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test',
-  force  => true,
+exec { 'f':
+  command => '/bin/ln -sf /data/web_static/releases/test /data/web_static/current',
 }
 
-exec { 'configure_nginx':
+exec { 'h':
   command => '/bin/sed -i "/listen 80 default_sever/a location /hbnb_static/ {alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
-  require => Exec['install_nginx'],
-  notify  => Exec['restart_nginx'],
 }
 
-exec { 'restart_nginx':
-  command     => '/usr/sbin/service nginx restart',
-  refreshonly => true,
+exec { 'i':
+  command => '/usr/sbin/service nginx restart',
 }
 
-exec { 'set_permissions':
+exec { 'g':
   command => '/bin/chown -R ubuntu:ubuntu /data',
 }
